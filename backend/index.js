37650caeaf44cbd6043436cc6833b543ec237b10
RamanShakya <Raman992@users.noreply.github.com>
2025-10-26 22:06:12 +0545

    dbadded

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const connectToMongo = require('./database');
-const express = require('express')
+const express = require('express');
+
 
 connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+
+app.use('/api/auth',require('./Routes/auth'));
+app.use('/api/notes',require('./Routes/notes'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
