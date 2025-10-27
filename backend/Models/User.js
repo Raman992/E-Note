904b25ba4b1aa9f79fcccf8eb86f5f1d8fb21ed5
RamanShakya <Raman992@users.noreply.github.com>
2025-10-27 16:21:42 +0545

    added hash and jwt

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const UserSchema = new Schema({
+  
   name: {
     type: String,
     required: true
@@ -21,5 +22,6 @@ const UserSchema = new Schema({
 }
 
 });
-
-module.exports = mongoose.model('user', UserSchema);
+const User = mongoose.model('user', UserSchema);
+User.createIndexes();
+module.exports = User;
--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -1,12 +1,55 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../Models/User');
-// Route 1: Test endpoint
-router.get('/', (req, res) => {
-  console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body);
-});
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const JWT_SIGNATURE = 'Londonstop';
+
+// Route: POST /api/auth . / No login required.
+router.post(
+  '/',
+  [
+    body('name', 'Invalid Name').isLength({ min: 4 }),
+    body('email', 'Invalid Email').isEmail(),
+    body('password', 'Password must be at least 6 characters').isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      
+      // Checking for duplicate email user
+      let ExistingUser = await User.findOne({ email: req.body.email });
+      if (ExistingUser) {
+        return res.status(400).json({ error: "Sorry a user with this email already exists" })
+      }
+      //Creating hash password
+        const salt = await bcrypt.genSalt(10);
+        const bcryptedpass = await bcrypt.hash(req.body.password , salt);
+      
+        //Creating a new user in database
+      const user = await User.create({
+        name: req.body.name,
+        password: bcryptedpass,
+        email: req.body.email
+      });
+      //Creating jwt payload
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+
+      const authtoken = jwt.sign(data, JWT_SIGNATURE);
+      //Sent success code with user authentication token
+      res.status(200).json({ message: 'User created successfully', authtoken });
+    } catch (error) {
+      res.status(500).json({ error: error.message }); //Error msg on body
+    }
+  }
+);
 
 module.exports = router;
