2921a60efc910dde6c5b29d48b59a31e96ea1a60
RamanShakya <Raman992@users.noreply.github.com>
2025-10-30 13:20:25 +0545

    added pages(components)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import './App.css'
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
+import Home from './components/home'
+import About from './components/about'
+import Navbar from './components/navbar'
+import Contact from './components/contact'
+
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
