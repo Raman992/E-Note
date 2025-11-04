046e3f5e3b52e9a32097a4776d9ad9fb04e5268a
RamanShakya <Raman992@users.noreply.github.com>
2025-11-04 20:22:59 +0545

    notestate

--- src/context/notes/noteState.jsx
+++ src/context/notes/noteState.jsx
@@ -17,30 +17,24 @@ const NoteState = (props) => {
   const fetchNotes = async () => {
     const response = await fetch(`${host}/api/notes/fetchnotes`, {
       method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjkwMWRjMDNkNTZkYmU5NTBiZWZjZjBkIn0sImlhdCI6MTc2MTcyOTU2N30.2bNXrXHyfqhvaY_I3elSMHMhiWBtCFGPYBetjAyyoiA"
-      }
+      headers: getAuthHeaders()
     });
     const json = await response.json()
     setNotes(json)
   }
 
-const addNotes = async (name, description, tag) => {
-  console.log("Sending:", { name, description, tag }); 
-
+  //Addnote
+  const addNotes = async (name, description, tag) => {
   const response = await fetch(`${host}/api/notes/createnotes`, {
     method: 'POST',
     headers: getAuthHeaders(),
     body: JSON.stringify({ name, description, tag })
   });
-
   if (!response.ok) {
     const err = await response.json();
     alert(err.error || "Failed to add note");
     return;
   }
-
   const newNote = await response.json();
   setNotes(notes.concat(newNote));
 };
@@ -51,10 +45,7 @@ const addNotes = async (name, description, tag) => {
     setNotes(newNotes);
     const response = await fetch(`${host}/api/notes/deletenotes/${id}`, {
       method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjkwMWRjMDNkNTZkYmU5NTBiZWZjZjBkIn0sImlhdCI6MTc2MTcyOTU2N30.2bNXrXHyfqhvaY_I3elSMHMhiWBtCFGPYBetjAyyoiA"
-      }
+      headers: getAuthHeaders()
     });
     const json = await response.json()
     console.log(json);
@@ -64,21 +55,21 @@ const addNotes = async (name, description, tag) => {
   const updateNotes = async (name, description, tag, id) => {
     const response = await fetch(`${host}/api/notes/updatenotes/${id}`, {
       method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjkwMWRjMDNkNTZkYmU5NTBiZWZjZjBkIn0sImlhdCI6MTc2MTcyOTU2N30.2bNXrXHyfqhvaY_I3elSMHMhiWBtCFGPYBetjAyyoiA"
-      },
+      headers: getAuthHeaders(),
       body: JSON.stringify({name, description, tag})
     });
     const json = await response.json()
     console.log(json)
-    for (let i = 0; i < notes.length; i++) {
-        if (notes[i]._id === id) {
-            notes[i].name = name;
-            notes[i].description = description;
-            notes[i].tag = tag;
+    let updated = JSON.parse(JSON.stringify(notes))
+    for (let i = 0; i < updated.length; i++) {
+        if (updated[i]._id === id) {
+            updated[i].name = name;
+            updated[i].description = description;
+            updated[i].tag = tag;
+            updated[i].date = new Date();
             break;
         }
+      setNotes(updated)
     }
   }
 
