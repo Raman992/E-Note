e33f3586e62483fa067ea8b22b1cc2ba9b4ee4be
RamanShakya <Raman992@users.noreply.github.com>
2025-11-04 21:07:49 +0545

    fixed

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import Notes from './notes'
 import Addnote from './addnote'
 
-import Alert from "./alert"
 export const Home = () => {
 
     return (
         <div>
-            <Alert/>
             <Addnote/>
             <Notes/>
         </div>
